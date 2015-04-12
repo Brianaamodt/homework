c13f912ff3ac1b07d8c439355f93e8c34feffcbb
Brianaamodt <brian.aamodt@gmail.com>
2015-04-12 14:16:58 -0500

    Delete singularity.js

--- singularity.js
+++
@@ -1,8 +0,0 @@
-function Dave(){
-	var fatality = prompt ("type anything object and i will tell you how long until is succumbs to the singularity")
-		if fatality(typeof(fatality) == Hope){
-			return ("No Hope! It has already begun!");}
-		} else {
-			var fatality = str.lenth;
-			return (fatality + " seconds until full integration");
-}
